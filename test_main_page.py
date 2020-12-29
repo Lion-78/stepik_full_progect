678c2f123bd7e41af8eaa90c6088831f8cc9f4e9
Oplima <oplima@yandex.ru>
2020-12-29 14:03:38 +0300

    Rewrite like Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,9 @@
+link = "http://selenium1py.pythonanywhere.com/"
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+   browser.get(link)
+   go_to_login_page(browser)
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
